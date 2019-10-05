da92ec5b7e103115c816c407c95459c1f82873cc
Diego <diegoluizjava.java@gmail.com>
2019-10-05 16:36:20 -0300

    Fixes

--- src/main/java/com/example/realworld/infrastructure/verticles/UsersAPIVerticle.java
+++ src/main/java/com/example/realworld/infrastructure/verticles/UsersAPIVerticle.java
@@ -2,11 +2,14 @@ package com.example.realworld.infrastructure.verticles;
 
 import com.example.realworld.domain.entity.persistent.User;
 import com.example.realworld.domain.service.UsersService;
+import com.example.realworld.infrastructure.Constants;
 import com.example.realworld.infrastructure.web.model.request.NewUserRequest;
 import com.example.realworld.infrastructure.web.model.response.UserResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.Promise;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
@@ -14,6 +17,8 @@ import io.vertx.ext.web.handler.BodyHandler;
 import javax.validation.Validator;
 
 public class UsersAPIVerticle extends AbstractAPIVerticle {
+
+  private final Logger logger = LoggerFactory.getLogger(getClass());
 
   private final String API_PATH = "/users";
 
@@ -37,6 +42,8 @@ public class UsersAPIVerticle extends AbstractAPIVerticle {
         router,
         httpServerAsyncResult -> {
           if (httpServerAsyncResult.succeeded()) {
+            logger.info(
+                "UsersAPI started on port " + config().getInteger(Constants.SERVER_PORT_KEY));
             startPromise.complete();
           } else {
             startPromise.fail(httpServerAsyncResult.cause());
