13fa9456e6ecb2b57c282edd0b8cdeff401ca219
diegocamara <diegoluiz.cjava@gmail.com>
2020-01-26 17:30:53 -0300

    Trying to fix ci build - 2

--- ci-build.sh
+++ ci-build.sh
@@ -2,9 +2,10 @@
 ./mvnw clean package
 java -jar target/vertx-springboot-realworld-example-app.jar > service.log &
 SERVICE_PROCESS=$!
-tail -f -n0 service.log | grep -q "Started RealworldApplication"
-echo "Application started"
+tail -f -n0 service.log
+#| grep -q "Started RealworldApplication"
+#echo "Application started"
 #./collections/run-api-tests.sh
-kill $SERVICE_PROCESS
-rm service.log
+#kill $SERVICE_PROCESS
+#rm service.log
 
