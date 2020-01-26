690fca7424f8bbc19d6e77ca22de78ca872dec04
Diego <diegoluizjava.java@gmail.com>
2020-01-25 22:53:46 -0300

    Fix ci script

--- ci-build.sh
+++ ci-build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 ./mvnw clean package
-java -jar target/vertx-springboot-realworld-example-app.jar
+java -jar target/vertx-springboot-realworld-example-app.jar > service.log &
+SERVICE_PROCESS=$!
+tail -f service.log | grep -q 'Started RealworldApplication'
+echo "Application started"
+./collections/run-api-tests.sh
+kill $SERVICE_PROCESS
+rm service.log
 
